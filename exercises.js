cd5f4de9cec4e2e6208e5412436689072038d824
Lance Shaw <AmbroseSouth@outlook.com>
2022-01-25 16:39:15 -0600

    completed

---
+++ exercises.js
@@ -0,0 +1,24 @@
+// function double(arr) {
+//     return arr.map(function (val) {
+//         return val * 2;
+//     });
+// }
+
+let double = (arr) => { return arr.map((val) => { return val * 2 }) }
+
+
+
+
+// function squareAndFindEvens(numbers) {
+//     var squares = numbers.map(function (num) {
+//         return num ** 2;
+//     });
+//     var evens = squares.filter(function (square) {
+//         return square % 2 === 0;
+//     });
+//     return evens;
+// }
+
+
+
+let squareAndFindEvens = (numbers) => { return numbers.map((n) => n ** 2).filter((s) => s % 2 === 0) }
